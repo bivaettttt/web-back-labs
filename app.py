2def9527b2f5718da56388531a1a4043114ade9c
Тимур Надршин <nadrshin.timur@mail.ru>
2025-10-08 01:26:29 +0700

    Имя студента

--- app.py
+++ app.py
@@ -457,4 +457,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Тимур Надршин'
+    return render_template('example.html', name=name)
