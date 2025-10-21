2fe96810c10100e0108a925777107ac84265b40f
Тимур Надршин <nadrshin.timur@mail.ru>
2025-10-22 02:15:25 +0700

    Очистка всех кук в settings

--- lab3.py
+++ lab3.py
@@ -180,3 +180,13 @@ def ticket_result():
                         age=age, departure=departure, destination=destination,
                         date=date, insurance=insurance, ticket_type=ticket_type,
                         price=price)
+
+
+@lab3.route('/lab3/settings_clear')
+def settings_clear():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('text_align')
+    return resp
