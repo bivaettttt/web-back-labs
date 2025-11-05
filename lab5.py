2e6cc718cd1868e1de9c5cf6d49e9e55fb332f12
Тимур Надршин <nadrshin.timur@mail.ru>
2025-11-05 16:47:17 +0700

    Заготовка к 5-ой лабораторной

---
+++ lab5.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    return render_template('lab5/lab5.html')
+
+@lab5.route('/lab5/login')
+def login():
+    return "Страница входа"
+
+@lab5.route('/lab5/register')
+def register():
+    return "Страница регистрации"
+
+@lab5.route('/lab5/list')
+def articles_list():
+    return "Список статей"
+
+@lab5.route('/lab5/create')
+def create_article():
+    return "Создать статью"
