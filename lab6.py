e5f62ca06874ad1fd9f2619effbff4d0ec03e284
Тимур Надршин <nadrshin.timur@mail.ru>
2025-12-02 15:39:44 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -56,6 +56,41 @@ def api():
                     'id': id
                 }
 
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+
+        for office in offices:
+            if office['number'] == office_number:
+                # Офис не арендован
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+
+                # Офис арендован другим пользователем
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Cannot cancel someone else\'s booking'
+                        },
+                        'id': id
+                    }
+
+                # Всё ок – снимаем аренду
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+
     return {
         'jsonrpc': '2.0',
         'error': {
