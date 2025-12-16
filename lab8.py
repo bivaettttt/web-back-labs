005be4ea91d7771ce3bf98d2d2033831a35e429a
Тимур Надршин <nadrshin.timur@mail.ru>
2025-12-16 23:21:11 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint("lab8", __name__, template_folder="templates")
@@ -37,3 +38,36 @@ def register():
 
     # GET или ошибка — возвращаем форму + сообщение
     return render_template('lab8/register.html', error=error)
+
+
+@lab8.route('/lab8/login', methods=['GET', 'POST'])
+def login():
+    error = None
+
+    if request.method == 'POST':
+        login_form = (request.form.get('login') or '').strip()
+        password_form = request.form.get('password') or ''
+
+        # Проверки на непустые значения
+        if login_form == '':
+            error = 'Имя пользователя не должно быть пустым'
+        elif password_form == '':
+            error = 'Пароль не должен быть пустым'
+        else:
+            user = users.query.filter_by(login=login_form).first()
+
+            if user:
+                if check_password_hash(user.password, password_form):
+                    login_user(user, remember = False)
+                    return redirect('/lab8/')
+
+            # если пользователь не найден ИЛИ пароль неверен
+            error = 'Ошибка входа: логин и/или пароль неверны'
+
+    return render_template('lab8/login.html', error=error)
+
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
