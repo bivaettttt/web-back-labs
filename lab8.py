6f70e41214dc3b8a3b6b2dfc7b22947f92a108de
Тимур Надршин <nadrshin.timur@mail.ru>
2025-12-16 22:22:07 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,7 +1,39 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint("lab8", __name__, template_folder="templates")
 
 @lab8.route("/lab8/")
 def main():
     return render_template("lab8/index.html", username="anonymous")
+
+
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+
+    if request.method == 'POST':
+        login_form = (request.form.get('login') or '').strip()
+        password_form = request.form.get('password') or ''
+
+        # Проверки из методички
+        if login_form == '':
+            error = 'Имя пользователя не должно быть пустым'
+        elif password_form == '':
+            error = 'Пароль не должен быть пустым'
+        else:
+            # Проверка существования пользователя
+            login_exists = users.query.filter_by(login=login_form).first()
+            if login_exists:
+                error = 'Такой пользователь уже существует'
+            else:
+                password_hash = generate_password_hash(password_form)
+                new_user = users(login=login_form, password=password_hash)
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect('/lab8/')
+
+    # GET или ошибка — возвращаем форму + сообщение
+    return render_template('lab8/register.html', error=error)
